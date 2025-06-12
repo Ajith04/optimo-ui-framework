e8b22e94ba75fed2fd99ea8233da4127a7bf09fa
Anojan <ucauser@programus.co.uk>
2025-06-12 11:59:09 +0530

    Implement DashBord Show our Components and add modal bar

--- src/js/optimo.js
+++ src/js/optimo.js
@@ -1,1 +1,24 @@
-import 'components/modal';
+
+    document.querySelectorAll('.nav-link').forEach(link => {
+    
+      const linkUrl = new URL(link.href, window.location.origin);
+      if (linkUrl.pathname === window.location.pathname) {
+        link.classList.add('active');
+      }
+    });
+  
+  document.getElementById("openModalBtn").onclick = function () {
+    document.getElementById("myModal").classList.add("active");
+  };
+  
+  document.querySelectorAll(".closeModalBtn").forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      document.getElementById("myModal").classList.remove("active");
+    });
+  });
+  
+  
+  document.querySelector(".modal__backdrop").onclick = function () {
+    document.getElementById("myModal").classList.remove("active");
+  };
+  
